7f5c70b341cf66c8a5bb7841d6e80afc80d852e3
ygonb <ygo@pesqueira.ifpe.edu.br>
2020-04-11 01:05:49 -0300

    Testes com encoders UTF-8, ISO-8859-1, etc (4h) Antes disso: 4h nos testes iniciais para apresentação da ideia na reunião do NDE 8h Na elaboração das classes Aluno e CompoCurr 4h Inclusão da característica do algoritmo de desistencia de alunos

--- academicflow/src/main/java/br/edu/ifpe/academicflow/Main.java
+++ academicflow/src/main/java/br/edu/ifpe/academicflow/Main.java
@@ -1,8 +1,12 @@
 package br.edu.ifpe.academicflow;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.Random;
 import java.util.Arrays;
@@ -11,6 +15,9 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.io.IOException;
+
+import static java.nio.charset.StandardCharsets.ISO_8859_1;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 class Main {
     public static void main(String[] args) throws IOException {
@@ -22,10 +29,9 @@ class Main {
         int totalEgressos[] = new int[10];
         int totalDesistentes[] = new int[10];
         boolean tmpEgressos;
-        FileWriter fw = new FileWriter("out.txt");
+        //FileWriter fw = new FileWriter("out.txt");
+        OutputStreamWriter fw = new OutputStreamWriter(new FileOutputStream("out.txt"),"ISO-8859-1");
         PrintWriter pw = new PrintWriter(fw);
-        FileReader fr = new FileReader("out.txt");
-        BufferedReader br = new BufferedReader(fr);
 
 
         for (int indAlunos=0; indAlunos<=99; indAlunos++){
@@ -86,7 +92,7 @@ class Main {
             for (int indAlunos=0; indAlunos<=99; indAlunos++){
                 alunos[indAlunos].cursaPeriodo(compsCurr);}
 
-            // Verifica aprovaÃ§Ã£o
+            // Verifica aprovação
             ptoCorte(periodo-1,compsCurr);
             for (int indAlunos=0; indAlunos<=99; indAlunos++){
                 alunos[indAlunos].verificaAprov(compsCurr);}
@@ -95,7 +101,7 @@ class Main {
 
 
         pw.println("===================");
-        pw.println("MATRÃCULAS POR COMPONENTE ( <-PERÃODOS-> )");
+        pw.println("MATRÍCULAS POR COMPONENTE ( <-PERÍODOS-> )");
 
         for (int indComp=0; indComp<=5; indComp++){
             pw.printf("%s ",compsCurr[indComp].cod);
@@ -123,26 +129,23 @@ class Main {
 
         pw.close();
 
+        InputStreamReader fr = new InputStreamReader(new FileInputStream("out.txt"), "ISO-8859-1");
+        BufferedReader br = new BufferedReader(fr);
+
         String linha = br.readLine();
         while (linha!=null){
         System.out.println(linha);
         linha = br.readLine();}
-
-
     }
 
 
-
-
-
-
     static CompCurr[] geraMatriz(){
-        CompCurr FEL = new CompCurr(0,"FEL","Fundamentos de EletrotÃ©cnica",1,40,80,"","",-1,-1);
-        CompCurr CA1 = new CompCurr(1,"CA1","CÃ¡lculo I",1,40,60,"","",-1,-1);
-        CompCurr FI2 = new CompCurr(2,"FI2","FÃ­sica II",2,40,70,"FEL","CA2",0,3);
-        CompCurr CA2 = new CompCurr(3,"CA2","CÃ¡lculo II",2,40,50,"CA1","",1,-1);
-        CompCurr ET1 = new CompCurr(4,"ET1","EletrÃ´nica I",3,40,80,"FI2","",2,-1);
-        CompCurr LE1 = new CompCurr(5,"LE1","LaboratÃ³rio de EletrÃ´nica I",3,20,90,"","ET1",-1,4);
+        CompCurr FEL = new CompCurr(0,"FEL","Fundamentos de Eletrotécnica",1,40,80,"","",-1,-1);
+        CompCurr CA1 = new CompCurr(1,"CA1","Cálculo I",1,40,60,"","",-1,-1);
+        CompCurr FI2 = new CompCurr(2,"FI2","Física II",2,40,70,"FEL","CA2",0,3);
+        CompCurr CA2 = new CompCurr(3,"CA2","Cálculo II",2,40,50,"CA1","",1,-1);
+        CompCurr ET1 = new CompCurr(4,"ET1","Eletrônica I",3,40,80,"FI2","",2,-1);
+        CompCurr LE1 = new CompCurr(5,"LE1","Laboratório de Eletrônica I",3,20,90,"","ET1",-1,4);
         CompCurr compsCurr[] = {FEL,CA1,FI2,CA2,ET1,LE1};
         return compsCurr;
     }
